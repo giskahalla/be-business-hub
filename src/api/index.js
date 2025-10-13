114dc3921cf6cfbd9f746310a14f9a341333c9fc
Giska <giskahalla123@gmail.com>
2025-10-13 07:13:26 +0700

    update 13/10/25
    - Add Project GET, CREATE, and UPDATE APIs
    - Add GET Company and Member APIs

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import { readFileSync, writeFileSync } from 'fs';
 
-import { generateCustomerId } from '../handler/index.js';
+import { generateCustomerId, generateProjectId } from '../handler/index.js';
 
 
 const app = express()
@@ -13,6 +13,8 @@ let db = JSON.parse(readFileSync(dbFile, "utf-8"));
 
 const customers = db.customers;
 const projects = db.projects;
+const companies = db.companies;
+const members = db.members;
 
 app.use(express.json());
 
@@ -58,9 +60,47 @@ app.use(function(req, res, next) {
     
   });
 
-    app.get('/get/projects', (req, res) => {
-      res.json({projects: projects});
-    });
+  app.get('/get/projects', (req, res) => {
+    res.json({projects: projects});
+  });
+
+  app.post('/create/project', (req, res) => {
+    const newProject = {
+      ...req.body.data,
+      id: generateProjectId(projects),
+      status: 1
+      
+    };
+    
+    projects.push(newProject);
+    writeFileSync(dbFile, JSON.stringify(db, null, 2));
+    
+    res.json({project: newProject});
+  });
+
+   app.post('/update/project', (req, res) => {
+    const { data } = req.body
+
+     const projectIndex = projects.findIndex(project => project.id === data.id);
+    
+    if (projectIndex === -1) {
+        return res.status(404).json({ message: 'Project not found' });
+    }
+    
+    projects[projectIndex] = { ...projects[projectIndex], ...data };
+
+    writeFileSync(dbFile, JSON.stringify(db, null, 2));
+    res.json({project: projects[projectIndex]});
+    
+  });
+
+  app.get('/get/companies', (req, res) => {
+    res.json({companies: companies});
+  });
+
+  app.get('/get/members', (req, res) => {
+    res.json({members: members});
+  });
       
 app.listen(5050, () => {})
 
--- src/handler/index.js
+++ src/handler/index.js
@@ -9,3 +9,14 @@ export const generateCustomerId = (customers) => {
 
   return `CUS${String(maxNumber + 1).padStart(3, "0")}`;
 }
+
+export const generateProjectId = (projects) => {
+  if (projects.length === 0) return "PRJ001";
+
+  const maxNumber = projects.reduce((max, c) => {
+    const num = parseInt(c.id.replace("PRJ", ""), 10);
+    return num > max ? num : max;
+  }, 0);
+
+  return `PRJ${String(maxNumber + 1).padStart(3, "0")}`;
+}
