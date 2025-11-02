fee155df41a7f81fe41e18a4952ef33c46cc4d2e
Giska <giskahalla123@gmail.com>
2025-11-02 18:06:47 +0700

    update 02/11/25
    - Improve project create/update logic to also update member and customer data

--- src/api/index.js
+++ src/api/index.js
@@ -35,7 +35,6 @@ app.use(function(req, res, next) {
       ...req.body.data,
       id: generateCustomerId(customers),
       status: 2
-      
     };
     
     customers.push(newCustomer);
@@ -64,30 +63,100 @@ app.use(function(req, res, next) {
     res.json({projects: projects});
   });
 
+  app.get('/get/project/thru/userId', (req, res) => {
+    const { id } = req.query; 
+
+    let project = [];
+
+    if(id.includes("MEM")) {
+      project = projects.filter(prj => prj.assignee === id); 
+    } else {
+      project = projects.filter(prj => prj.client === id);
+    }
+
+    res.json({projects: project});
+  });
+
   app.post('/create/project', (req, res) => {
+    const { data } = req.body
+    const { assignee, client, budget } = data || {}
+
+    if (!data || !client || !assignee) {
+      return res.status(400).json({ error: "Missing required fields: client or assignee" });
+    }
+
     const newProject = {
-      ...req.body.data,
+      ...data,
       id: generateProjectId(projects),
-      status: 1
-      
+      status: 1,
+      budget: { estimated: budget || 0 }
     };
-    
+
+    if (client) {
+      const customer = customers.find(cust => cust.id === client); 
+      if (customer) {
+        if (!customer.projects) {
+          customer.projects = []; 
+        }
+        customer.projects.push(newProject.id);
+      }
+    }
+
+    if (assignee) {
+      const member = members.find(mem => mem.id === assignee); 
+      if (member) {
+        if (!member.projects) {
+          member.projects = []; 
+        }
+        member.projects.push(newProject.id);
+      }
+    }
+
     projects.push(newProject);
+
+    const db = { projects, customers, members };
     writeFileSync(dbFile, JSON.stringify(db, null, 2));
-    
-    res.json({project: newProject});
+
+    res.json({ project: newProject });
   });
 
    app.post('/update/project', (req, res) => {
     const { data } = req.body
+    const { assignee, client, spent } = data || {}
 
-     const projectIndex = projects.findIndex(project => project.id === data.id);
+    const projectIndex = projects.findIndex(project => project.id === data.id);
     
     if (projectIndex === -1) {
         return res.status(404).json({ message: 'Project not found' });
     }
     
-    projects[projectIndex] = { ...projects[projectIndex], ...data };
+    projects[projectIndex] = { ...projects[projectIndex], ...data, budget: {
+        ...projects[projectIndex].budget.estimated,
+        used: spent !== undefined ? spent : projects[projectIndex].budget.spent,
+      } 
+    };
+
+    const newProjectId = data.id;
+
+    if (client) {
+        if (!customers[client].projects) {
+            customers[client].projects = [];
+        }
+        customers[client] = {
+            ...customers[client],
+            projects: [...customers[client].projects, newProjectId],
+        };
+    }
+
+    if (assignee) {
+        if (!members[assignee].projects) {
+            members[assignee].projects = [];
+        }
+        members[assignee] = {
+            ...members[assignee],
+            projects: [...members[assignee].projects, newProjectId],
+        };
+    }
 
     writeFileSync(dbFile, JSON.stringify(db, null, 2));
     res.json({project: projects[projectIndex]});
